e2a66b8beb57f362b800994bda3f141688657b90
Paul <psholloway@me.com>
2023-07-27 21:44:09 +1000

    Centre aligned top nav bar

--- Views/home.php
+++ Views/home.php
@@ -3,9 +3,26 @@
 <head>
 <meta charset="utf-8">
 <title>Home</title>
+	<link href="styles.css" rel="stylesheet">
 </head>
 
 <body>
-	<p>Home Page</p>
+	<header>
+		<div class="topnav">
+  			<a class="active" href="#home">Home</a>
+  			<a href="#news">News</a>
+  			<a href="#contact">Contact</a>
+  			<a href="#about">About</a>
+		</div>
+	</header>
+	
+	<main>
+		<h1>Home Page</h1>
+	</main>
+	
+	<footer>
+		<p>Author: Paul Holloway</p>
+  		<p><a href="mailto:pholloway@mail.com">pholloway@mail.com</a></p>
+	</footer>
 </body>
 </html>
